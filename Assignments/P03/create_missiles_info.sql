9a579c058701e6f53233eaec12916a703a8406e0
pramey thumar <prameythumar3@gmail.com>
2022-10-05 11:33:21 -0500

    p03 complete

---
+++ Assignments/P03/create_missiles_info.sql
@@ -0,0 +1,7 @@
+-- Creating table: missiles_info 
+CREATE TABLE IF NOT EXISTS public.missiles_info
+(
+    name text COLLATE pg_catalog."default",
+    blast_radius real,
+    speed real
+);
---
+++ Assignments/P03/create_missiles_position.sql
@@ -0,0 +1,27 @@
+-- Creating table: missiles_position 
+create table if not exists missiles_position(name text, starts geometry, ends geometry, altitude smallint, time_stamp time); 
+
+
+--Generate points on the top and bottom of bbox.
+SELECT ST_x((ST_Dump(
+                  ST_GeneratePoints(geom, 20, 1996)
+                  )).geom)as x,
+              ST_y((ST_Dump(
+                  ST_GeneratePoints(geom, 20, 1996)
+                  )).geom) as y
+FROM (
+SELECT ST_Buffer(
+	ST_GeomFromText('SRID=4326;LINESTRING(-129.784 19.743,-61.951 19.743, -61.951 54.345,-129.784 54.345)'),
+	3,
+	'endcap=round join=round')  As geom 
+) as it;
+
+
+-- Inserting missiles data from generated points.
+insert into missiles_position
+	values(
+    	'${generate_name()}', 
+        ST_SetSRID(ST_MakePoint(${data[i].x}, ${data[i].y}),4326),
+        ST_SetSRID(ST_MakePoint(${data[data.length / 2 + i].x}, ${data[data.length / 2 + i].y}),4326),Math.floor(Math.random() * (12000 - 10000) + 10000)},
+       	current_time
+    );
